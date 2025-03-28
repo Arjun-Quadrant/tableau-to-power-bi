536e622da9a00cdfcb3652547287498f2bf0dcd4
Arjun-Quadrant <arjun.n@quadranttechnologies.com>
2025-03-28 13:09:49 -0700

    Add a template configuration file

---
+++ config_template.py
@@ -0,0 +1,22 @@
+tableau_server = ""
+api_version = ""
+site_name = ""  # Leave empty for the default site
+# Azure App Info
+client_id = ""
+tenant_id = ""
+workspace_id = ""
+# Metadata extraction info
+workbook_path=r""
+metadata_folder_path = r""
+metadata_file_name = ""
+# Fabric configuration
+workspace_name = ""
+lakehouse_name = ""
+lakehouse_table_name = ""
+lakehouse_metadata_directory = ""
+semantic_model_name = ""
+semantic_model_folder = r""
+report_name = ""
+report_folder = r""
+auth_username=""
+auth_password=""
